1c0f229c786efd81de8f74318ccf5f8de43f05fe
Damilola Oduronbi <doduronbi@gmail.com>
2019-01-18 16:55:18 +0100

    after reset

---
+++ app/build/precache-manifest.594468a1e70656b96196d0c9b8ecd840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "10256b2700850e285109",
+    "url": "/app/static/js/runtime~main.10256b27.js"
+  },
+  {
+    "revision": "2272628149ee5e3d11b6",
+    "url": "/app/static/js/main.22726281.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "2272628149ee5e3d11b6",
+    "url": "/app/static/css/main.e1ce6017.chunk.css"
+  },
+  {
+    "revision": "36cde1ea4dbcacb99f78c0767ff6829c",
+    "url": "/app/index.html"
+  }
+];
